610261707e9fb63195d75b8490466647dfbaf86f
yangxs <xios-young@qq.com>
2024-11-20 15:44:39 +0800

    适配有死角的图片

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # encoding: utf-8
+import logging
 import os.path
 import sys
 
 import cv2
+
+logging.basicConfig(level=logging.INFO)
 
 
 class Mazer:
@@ -24,7 +27,7 @@ class Mazer:
         # ↓1 ←2 ↑3 →4  左转减一，右转加一，如果是0就改成4，如果是5就改成1
         self.dir = 4
         # self.org_img[row, col, cell], 但是这里是反过来的 (col, row)
-        self.pos = [0, (int)(self.height/2)]
+        self.pos = [0, (int)(self.height / 2)]
 
         self.start_touch = False
 
@@ -32,12 +35,12 @@ class Mazer:
         # ↓1
         if self.dir == 1:
             # 如果前方是红色的，可能是遇到死角掉头了，继续向前
-            if (self.get_color((self.pos[0], self.pos[1]+1)) == (0, 0, 255)).all():
+            if (self.get_color((self.pos[0], self.pos[1] + 1)) == (0, 0, 255)).all():
                 self.pos[1] = self.pos[1] + 1
             elif self.pos[1] == self.height - 1:
                 # 遇到边界右转
                 self.turn_right()
-            elif not self.check_pos_empty((self.pos[0], self.pos[1]+1)):
+            elif not self.check_pos_empty((self.pos[0], self.pos[1] + 1)):
                 # 如果前方不为空就右转
                 self.turn_right()
             else:
@@ -45,11 +48,11 @@ class Mazer:
         # ←2
         elif self.dir == 2:
             # 如果前方是红色的，可能是遇到死角掉头了，继续向前
-            if (self.get_color((self.pos[0]-1, self.pos[1])) == (0, 0, 255)).all():
+            if (self.get_color((self.pos[0] - 1, self.pos[1])) == (0, 0, 255)).all():
                 self.pos[0] = self.pos[0] - 1
             elif self.pos[0] == 0:
                 self.turn_right()
-            elif not self.check_pos_empty((self.pos[0]-1, self.pos[1])):
+            elif not self.check_pos_empty((self.pos[0] - 1, self.pos[1])):
                 # 如果前方不为空就右转
                 self.turn_right()
             else:
@@ -57,11 +60,11 @@ class Mazer:
         # ↑3
         elif self.dir == 3:
             # 如果前方是红色的，可能是遇到死角掉头了，继续向前
-            if (self.get_color((self.pos[0], self.pos[1]-1)) == (0, 0, 255)).all():
+            if (self.get_color((self.pos[0], self.pos[1] - 1)) == (0, 0, 255)).all():
                 self.pos[1] = self.pos[1] - 1
             elif self.pos[1] == 0:
                 self.turn_right()
-            elif not self.check_pos_empty((self.pos[0], self.pos[1]-1)):
+            elif not self.check_pos_empty((self.pos[0], self.pos[1] - 1)):
                 # 如果前方不为空就右转
                 self.turn_right()
             else:
@@ -69,11 +72,11 @@ class Mazer:
         # →4
         elif self.dir == 4:
             # 如果前方是红色的，可能是遇到死角掉头了，继续向前
-            if (self.get_color((self.pos[0]+1, self.pos[1])) == (0, 0, 255)).all():
+            if (self.get_color((self.pos[0] + 1, self.pos[1])) == (0, 0, 255)).all():
                 self.pos[0] = self.pos[0] + 1
             elif self.pos[0] >= self.width - 1:
                 self.turn_right()
-            elif not self.check_pos_empty((self.pos[0]+1, self.pos[1])):
+            elif not self.check_pos_empty((self.pos[0] + 1, self.pos[1])):
                 # 如果前方不为空就右转
                 self.turn_right()
             else:
@@ -82,26 +85,29 @@ class Mazer:
     def run(self):
         step = 1
         while True:
-            # print(step, self.pos, self.dir)
+            logging.debug("%s %s %s", step,  self.pos, self.dir)
             # 把当前位置涂红
             self.org_img[self.pos[1], self.pos[0]] = [0, 0, 255]
             if self.check_empty_left():
                 # 一开始左侧为空，但是还没碰到迷宫的墙
                 if self.start_touch:
-                    # print("empty left")
+                    logging.debug("empty left")
                     self.turn_left()
                 else:
-                    if not self.check_pos_empty((self.pos[0]+1, self.pos[1])):
+                    if not self.check_pos_empty((self.pos[0] + 1, self.pos[1])):
                         self.start_touch = True
             self.move()
             self.org_img[self.pos[1], self.pos[0]] = [0, 0, 255]
             if self.check_flag_left():
-                # self.show()
-                split_name = os.path.splitext(self.file_name)
-                # black_mask = (self.org_img[:, :, 0] == 0) & (self.org_img[:, :, 1] == 0) & (self.org_img[:, :, 2] == 0)
-                # self.org_img[black_mask] = (255, 255, 255)
-                cv2.imwrite(split_name[0] + "-r.jpg", self.org_img)
-                break
+                if self.check_white_left2():
+                    self.turn_left()
+                else:
+                    # self.show()
+                    split_name = os.path.splitext(self.file_name)
+                    # black_mask = (self.org_img[:, :, 0] == 0) & (self.org_img[:, :, 1] == 0) & (self.org_img[:, :, 2] == 0)
+                    # self.org_img[black_mask] = (255, 255, 255)
+                    cv2.imwrite(split_name[0] + "-r.jpg", self.org_img)
+                    break
 
             step = step + 1
             # if step % 10000 == 0:
@@ -141,28 +147,53 @@ class Mazer:
 
         return False
 
+    # 检查左侧的左侧是否是空白， 如果是空白就说明遇到了死胡同
+    def check_white_left2(self):
+        # ↓1
+        if self.dir == 1:
+            v = self.org_img[self.pos[1], self.pos[0]+2]
+            if v[0] == 255 and v[1] == 255 and v[2] == 255:
+                return True
+        # ←2
+        elif self.dir == 2:
+            v = self.org_img[self.pos[1]+2, self.pos[0]]
+            if v[0] == 255 and v[1] == 255 and v[2] == 255:
+                return True
+        # ↑3
+        elif self.dir == 3:
+            v = self.org_img[self.pos[1], self.pos[0]-2]
+            if v[0] == 255 and v[1] == 255 and v[2] == 255:
+                return True
+        # →4
+        elif self.dir == 4:
+            v = self.org_img[self.pos[1]-2, self.pos[0]]
+            if v[0] == 255 and v[1] == 255 and v[2] == 255:
+                return True
+
+        return False
+
     # 检查左侧是否为空，墙和界外False，空地True
     def check_empty_left(self):
         # ↓1
         if self.dir == 1:
-            if self.pos[0] >= self.width-1:
+            if self.pos[0] >= self.width - 1:
                 return False
-            return self.check_pos_empty((self.pos[0]+1, self.pos[1]))
+            return self.check_pos_empty((self.pos[0] + 1, self.pos[1]))
         # ←2
         elif self.dir == 2:
-            if self.pos[1] >= self.height-1:
+            if self.pos[1] >= self.height - 1:
                 return False
-            return self.check_pos_empty((self.pos[0], self.pos[1]+1))
+            return self.check_pos_empty((self.pos[0], self.pos[1] + 1))
         # ↑3
         elif self.dir == 3:
             if self.pos[0] <= 0:
                 return False
-            return self.check_pos_empty((self.pos[0]-1, self.pos[1]))
+            return self.check_pos_empty((self.pos[0] - 1, self.pos[1]))
         # →4
         elif self.dir == 4:
             if self.pos[1] <= 0:
                 return False
-            return self.check_pos_empty((self.pos[0], self.pos[1]-1))
+            return self.check_pos_empty((self.pos[0], self.pos[1] - 1))
 
     # 检查位置是否为空
     def check_pos_empty(self, pos):
@@ -196,6 +227,6 @@ if __name__ == "__main__":
             for file in files:
                 if not file.endswith(".png"):
                     continue
-                # print(file)
+                logging.debug(file)
                 mazer = Mazer(dir + "/" + file)
                 mazer.run()
